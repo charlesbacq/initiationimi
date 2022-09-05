6b0b994050a139924804ddec19fb8e193d71d4d9
Audrey Airaud <audrey.airaud@gmail.com>
2022-09-05 16:38:02 +0200

    message chiant

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import os
 import logging
 
-def mian():
+def main():
     logging.info("The script is now working in the directory: " + os.getcwd())
 
-if __name__ == "__mian__":
+if __name__ == "__main__":
     print("Hello, world")
-    mian()
+    main()
