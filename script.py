1930506cacbd71f9ffb8dcef17ec9b1b3808e220
charlesbacq <charles.bacquaert@orange.fr>
2022-09-05 16:24:03 +0200

    changement de nom de fonction

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import os
 import logging
 
-def main():
+def mian():
     logging.info("The script is now working in the directory: " + os.getcwd())
 
-if __name__ == "__main__":
+if __name__ == "__mian__":
     print("Hello world!")
-    main()
+    mian()
